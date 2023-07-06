bda6b8e9a2c5ad9f69878c86f80d364dab1b5352
sfia-o <sofia.vl.oliveira@gmail.com>
2023-07-06 06:03:35 +0000

    Implement characterCount function and call on input event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-  //select textarea element
+  //select elements
   const $textarea = $('#tweet-text');
+  const $output = $('output');
 
-  function handleKeypress(event) {
-     console.log('keypressed:', event);
+  //declare variable to store text length count
+  let textLength = 0;
+  
+  //function to count remaining characters
+  const characterCount = function() {
+    let charCount = 140 - textLength; //deducting the text length from the allowed character number
+    $output.text(charCount); //update output element
+
+    //condition to change text color to red when charCount is negative
+    if (charCount < 0) {
+      $output.css('color', 'red');
+    }
+
   }
+  
+  //create event handler for input event on textarea
+  $textarea.on('input', function() {
+    textLength = $(this).val().length;
+    characterCount(); //callback
+  });
 
-  //create an event handler
-  $textarea.on('keypress', handleKeypress);
 });
 
