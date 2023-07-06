1ba0c3a02f4eeb67bcf800d88748001f7efa2188
sfia-o <sofia.vl.oliveira@gmail.com>
2023-07-06 09:05:11 +0000

    Add hover effects to tweet box and icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  //select elements
+  const $tweetContainer = $('article');
+  
+  //create event handler for hover event
+  $tweetContainer.hover(
+    function() {
+      $(this).css('box-shadow', '4px 4px 4px');
+    },
+    function() {
+      $(this).css('box-shadow', 'none');
+    }
+  );
+
+  const $tweetIcons = $('.icons i');
+
+  $tweetIcons.hover(
+    function() {
+      $(this).css('color', '#D4A82A');
+    },
+    function() {
+      $(this).css('color', '#4056A1');
+    }
+  );
+
+
+
+
+
+
+});
+
