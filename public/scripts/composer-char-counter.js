8580db0eb68c816b486d14cd251ac49623ae32dc
sfia-o <sofia.vl.oliveira@gmail.com>
2023-07-06 05:43:07 +0000

    Initial commit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+  //select textarea element
+  const $textarea = $('#tweet-text');
 
+  function handleKeypress(event) {
+     console.log('keypressed:', event);
+  }
+
+  //create an event handler
+  $textarea.on('keypress', handleKeypress);
 });
+
