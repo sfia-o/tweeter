e915cd3ac745ff976eeff7a080a962cf7a44cc0d
sfia-o <sofia.vl.oliveira@gmail.com>
2023-07-11 22:44:10 +0000

    Change image sources

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -29,8 +29,8 @@ module.exports = {
    
     const avatars = {
     
-      Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
-      Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
+      Female: ["../images/girl.png", "../images/janeausten.png", "../images/woman.png", "../images/woman2.png"],
+      Male: ["../images/man (1).png","../images/man.png", "../images/man2.png", "../images/man3.png"]
     
     }
     
